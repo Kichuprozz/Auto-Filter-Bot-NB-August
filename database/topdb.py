78d15aef8d141302fcbf139f60ac88d61000b158
Kichuprozz <128233036+Kichuprozz@users.noreply.github.com>
2025-08-15 12:16:04 +0530

    Update topdb.py

--- database/topdb.py
+++ database/topdb.py
@@ -8,32 +8,32 @@ class Database:
         self.db = self.client[db_name]
         self.col = self.db.user        
 
-    async def update_top_messages(self, user_id, message_text):
-        user = await self.col.find_one({"user_id": user_id, "messages.text": message_text})
+    #async def update_top_messages(self, user_id, message_text):
+        #user = await self.col.find_one({"user_id": user_id, "messages.text": message_text})
         
-        if not user:
-            await self.col.update_one(
-                {"user_id": user_id},
-                {"$push": {"messages": {"text": message_text, "count": 1}}},
-                upsert=True
-            )
-        else:
-            await self.col.update_one(
-                {"user_id": user_id, "messages.text": message_text},
-                {"$inc": {"messages.$.count": 1}}
-            )
+        #if not user:
+            #await self.col.update_one(
+#                {"user_id": user_id},
+#                {"$push": {"messages": {"text": message_text, "count": 1}}},
+  #              upsert=True
+   #         )
+       # else:
+      #      await self.col.update_one(
+     #           {"user_id": user_id, "messages.text": message_text},
+     #           {"$inc": {"messages.$.count": 1}}
+   #         )
 
-    async def get_top_messages(self, limit=30):
-        pipeline = [
-            {"$unwind": "$messages"},
-            {"$group": {"_id": "$messages.text", "count": {"$sum": "$messages.count"}}},
-            {"$sort": {"count": -1}},
-            {"$limit": limit}
-        ]
-        results = await self.col.aggregate(pipeline).to_list(limit)
-        return [result['_id'] for result in results]
+    #async def get_top_messages(self, limit=30):
+        #pipeline = [
+        #    {"$unwind": "$messages"},
+    #        {"$group": {"_id": "$messages.text", "count": {"$sum": "$messages.count"}}},
+       #     {"$sort": {"count": -1}},
+    #        {"$limit": limit}
+#        ]
+ #       results = await self.col.aggregate(pipeline).to_list(limit)
+ #       return [result['_id'] for result in results]
     
-    async def delete_all_messages(self):
-        await self.col.delete_many({})
-
-silentdb = Database(DATABASE_URI, "SilentXBotz")
+ #   async def delete_all_messages(self):
+   #     await self.col.delete_many({})
+#
+#silentdb = Database(DATABASE_URI, "SilentXBotz")
